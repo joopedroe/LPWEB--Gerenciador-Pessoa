be565fe186c4c02e8a22ef9d3190aec00a188333
Joao Pedro Evangelista Rodrigues <joaopedrorodri@rede.ulbra.br>
2020-03-12 21:50:15 -0300

    inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Pessoa} from './models/Pessoa'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  nomePessoa=null;
+  idadePessoa=null;
+  id=0;
+  menorIdade:0;
+  maiorIdade:0;
+  
   title = 'gerenciarPessoa';
+  pessoa:Pessoa=new Pessoa();
+  menorPessoa:null;
+  maiorPessoa:null;
+
+  pessoas:Pessoa[]=[
+
+  ]
+
+  calculaIdade(idadeEntrada,pessoaEntrada){
+    if (this.id == 0){
+      this.menorIdade=idadeEntrada;
+      this.maiorIdade=idadeEntrada;
+      this.menorPessoa=this.maiorPessoa=pessoaEntrada;
+    }
+    else{
+      if (idadeEntrada<this.menorIdade){
+        this.menorPessoa=pessoaEntrada;
+        this.menorIdade=idadeEntrada;
+      }
+      if(idadeEntrada>this.maiorIdade){
+        this.maiorPessoa=pessoaEntrada;
+        this.maiorIdade=idadeEntrada;
+      }
+    }
+  }
+
+  adicionar(){
+    this.pessoas.push({id:this.id,nome:this.nomePessoa,idade:this.idadePessoa});
+    this.calculaIdade(this.idadePessoa,this.nomePessoa);
+    this.nomePessoa = null;
+    this.idadePessoa=null;
+    this.id+=1;
+    console.log(this.pessoas);
+  }
+
+
+  excluir(idExcluir){
+    this.pessoas.splice(idExcluir,1);
+  }
 }
